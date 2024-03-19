b4f8659973569a11e5d1465ff589720d88b0acb6
alvihelene <95623140+alvihelene@users.noreply.github.com>
2024-03-19 19:06:39 +0100

    File structure & 2 components created

---
+++ portfolio/src/components/background/Mountains.jsx
@@ -0,0 +1,11 @@
+import './Mountains.css'
+
+
+export default function Mountains(){
+    return(
+        <>
+        <div className="background"></div>
+            <div className="triangle"></div>
+        </>
+    );
+}
---
+++ portfolio/src/router/Router.jsx
@@ -0,0 +1,11 @@
+import Mountains from "../components/background/Mountains";
+import Navbar from "../nav/Navbar";
+
+export default function Router() {
+    return (
+        <>
+        <Navbar />
+        <Mountains />
+        </>
+    );
+}
