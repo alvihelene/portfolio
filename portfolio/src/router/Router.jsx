14704c7f42d5097ba74237e47f0740d98da40940
alvihelene <95623140+alvihelene@users.noreply.github.com>
2024-03-19 19:39:59 +0100

    Dynamic React routes prepared to be used
    "npm install react-router-dom@6" has been applied.
    
    Routes are now ready to be inserted in the Router.jsx for the navigationbar and possible portfolio content.

--- portfolio/src/router/Router.jsx
+++ portfolio/src/router/Router.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Mountains from "../components/background/Mountains";
 import Navbar from "../nav/Navbar";
 
 export default function Router() {
     return (
-        <>
-        <Navbar />
-        <Mountains />
-        </>
+        <BrowserRouter> 
+            <Navbar />
+            <Mountains />
+            
+                <Routes>
+                    <Route>
+
+                    </Route>
+                </Routes>
+        </BrowserRouter>
     );
 }
