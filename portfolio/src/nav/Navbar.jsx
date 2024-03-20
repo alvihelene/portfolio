8c233f908801264473dff581449502e604e2564a
alvihelene <95623140+alvihelene@users.noreply.github.com>
2024-03-20 10:09:14 +0100

    Navigation bar added with routes setup

--- portfolio/src/nav/Navbar.jsx
+++ portfolio/src/nav/Navbar.jsx
@@ -2,10 +2,21 @@ import { Link } from "react-router-dom"
 import "./Navbar.scss"
 
 export default function Navbar(){
+
+    const navLinks = [
+        { to: "/", label: "Alvi Møller" },
+        { to: "/portfolio", label: "Portfolio" },
+        { to: "/about", label: "Om" },
+        { to: "/contact", label: "Kontakt" },
+    ];
+
     return (
         <nav>
             <div className="nav-wrap">
+            {navLinks.map((link, index) => (
+                    <Link key={index} to={link.to}>{link.label}</Link>
+                ))}
             </div>
         </nav>
-    )
+    );
 }
--- portfolio/src/router/Router.jsx
+++ portfolio/src/router/Router.jsx
@@ -7,13 +7,30 @@ export default function Router() {
     return (
         <BrowserRouter> 
             <Navbar />
-            <Hej />
             <Mountains />
-                <Routes>
-                    <Route>
-
-                    </Route>
+            {/* <Hej /> */}
+                <Routes>    
+                    <Route path="/" element={<Home />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
                 </Routes>
         </BrowserRouter>
     );
+}
+
+function Home() {
+    return <h1>Forside</h1>;
+}
+
+function Portfolio() {
+    return <h1>Portfolio</h1>;
+}
+
+function About() {
+    return <h1>About Page</h1>;
+}
+
+function Contact() {
+    return <h1>Contact Page</h1>;
 }
