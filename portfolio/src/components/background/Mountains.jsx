a0cae9a0196be70126edba58f6ef716d92952cb6
alvihelene <95623140+alvihelene@users.noreply.github.com>
2024-03-22 07:46:25 +0100

    Mountains now responsive, but not pretty on mobile device

--- portfolio/src/components/background/Mountains.jsx
+++ portfolio/src/components/background/Mountains.jsx
@@ -10,10 +10,8 @@ export default function Mountains(){
             <div className="mountain-container">
                 <div className="mountain1"></div>
                 <div className="mountain2"></div>
-                <div className="mountain3-container">
                     <div className="mountain3"></div>
                     <div className="peak"></div>
-                    </div>
             </div>
         </div>
         </>
